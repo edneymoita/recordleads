cb0b54ab33b832f6e9a2aca81c33f103f780fc52
edneymoita <edneymoita@gmail.com>
2021-08-22 00:04:20 -0300

    Gravação no localStorage feita com sucesso!!!!

--- react/RecordLeads.tsx
+++ react/RecordLeads.tsx
@@ -1,36 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useCssHandles } from 'vtex.css-handles'
+
 
 const CSS_HANDLES = ['recordleads'] as const
 
 interface RecordLeads {
 }
 
+
 const RecordLeads: StorefrontFunctionComponent<RecordLeads> = ({ }) => {
+  
   const handles = useCssHandles(CSS_HANDLES)
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
 
   return (
     <>
+      <div className={`${handles.recordleads} flex flex-column mv3 items-center justify-center`}>
         <div className={`${handles.recordleads} flex flex-column mv3 items-center justify-center`}>
-          <div className={`${handles.recordleads} flex flex-column mv3 items-center justify-center`}>
-            <div className={`${handles.recordleads} c-on-base f4`}>Não fique fora dessa!</div>
-            <div className={`${handles.recordleads} c-on-base f5`}>Preencha os campos abaixo e seja o</div>
-            <div className={`${handles.recordleads} c-on-base f5`}>primeiro a saber tudo sobre essa mega promoção</div>
-          </div>
-          <div className={`${handles.recordleads} flex flex-row mv3 justify-around`}>
-            <label className={`${handles.recordleads} c-on-base f5`} htmlFor='nameBox'>Nome :</label>
-            <input className={`${handles.recordleads} c-on-base f5`} type='text' id='nameBox' placeholder='Fulano'/>
-          </div>
-          <div className={`${handles.recordleads} flex flex-row mv3 justify-around`}>
-            <label className={`${handles.recordleads} c-on-base f5`} htmlFor='emailBox'>E-mail :</label>
-            <input className={`${handles.recordleads} c-on-base f5`} type='email' id='emailBox' placeholder='fulano@algumlugar.com.br'/>
-          </div>
-          <div className={`${handles.recordleads} flex flex-row mv3 justify-around`}>
-            <label className={`${handles.recordleads} c-on-base f5`} htmlFor='phoneBox'>Fone :</label>
-            <input className={`${handles.recordleads} c-on-base f5`} type='phone' id='phoneBox' placeholder='(99)99999-9999'/>
-          </div>
-            <button className={`${handles.recordleads} c-on-base f5 pa3 br2 bg-emphasis`}>Faça meu cadastro</button>
+          <div className={`${handles.recordleads} c-on-base f4`}>Não fique fora dessa!</div>
+          <div className={`${handles.recordleads} c-on-base f5`}>Seja o primeiro a saber</div>
+          <div className={`${handles.recordleads} c-on-base f5`}>tudo sobre essa mega promoção</div>
         </div>
+        <div className={`${handles.recordleads} flex flex-row mv3 justify-around`}>
+          <input className={`${handles.recordleads} c-on-base f5`} type='text' id='nameBox' placeholder='Nome Completo' onChange={(e) => {setName(e.target.value)}}/>
+          </div><div className={`${handles.recordleads} flex flex-row mv3 justify-around`}>
+              <input className={`${handles.recordleads} c-on-base f5`} type='email' id='emailBox' placeholder='E-mail'  onChange={(e) => {setEmail(e.target.value)}}/>
+            </div><div className={`${handles.recordleads} flex flex-row mv3 justify-around`}>
+              <input className={`${handles.recordleads} c-on-base f5`} type='phone' id='phoneBox' placeholder='Telefone' onChange={(e) => {setPhone(e.target.value)}}/>
+            </div><button className={`${handles.recordleads} c-on-base--inverted f5 b pa3 bg-emphasis`} onClick={() => {
+              if (name != '') {
+                let client = {
+                  'name': name,
+                  'phone': phone,
+                  'email': email,
+                }
+
+                localStorage.setItem('client', JSON.stringify(client))
+              }
+            } }>Quero receber info!</button>
+      </div>
     </>
   )
 }
